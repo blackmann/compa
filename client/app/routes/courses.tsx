4a74c1b96d177831f4b9e6953dbdcb8757b96262
Yaw Twumasi Acheampong <auxibee@gmail.com>
2024-02-03 13:43:07 -0500

    remove post request method checks in actions

--- client/app/routes/courses.tsx
+++ client/app/routes/courses.tsx
@@ -3,20 +3,13 @@ import { prisma } from "~/lib/prisma.server";
 import { slugify } from "~/lib/slugify";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-	if (request.method !== "POST") {
-		return new Response(null, {
-			status: 405,
-			statusText: "Method Not Allowed",
-		});
-	}
+  const data = await request.json();
 
-	const data = await request.json();
+  const { code, name } = data;
 
-	const { code, name } = data;
+  await prisma.course.create({
+    data: { code, name, slug: slugify(`${code} ${name}`) },
+  });
 
-	await prisma.course.create({
-		data: { code, name, slug: slugify(`${code} ${name}`) },
-	});
-
-	return json({});
+  return json({});
 };
--- client/app/routes/programmes.tsx
+++ client/app/routes/programmes.tsx
@@ -3,19 +3,15 @@ import { prisma } from "~/lib/prisma.server";
 import { slugify } from "~/lib/slugify";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-	if (request.method !== "POST") {
-		throw new Response(null, { status: 405 });
-	}
+  const data = await request.json();
+  const programmeData = {
+    name: data.name,
+    slug: slugify(data.name),
+  };
 
-	const data = await request.json();
-	const programmeData = {
-		name: data.name,
-		slug: slugify(data.name),
-	};
+  const programme = await prisma.programme.create({
+    data: programmeData,
+  });
 
-	const programme = await prisma.programme.create({
-		data: programmeData,
-	});
-
-	return json({ programme });
+  return json({ programme });
 };
