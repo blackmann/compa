f8432c4b1975a09d4092bbaf5328c657a07a1daa
Yaw Twumasi Acheampong <auxibee@gmail.com>
2024-02-11 21:52:27 -0500

    add custom error page for unhandled errors/500

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -37,6 +37,38 @@ export const links: LinksFunction = () => [
 	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
+export function ErrorBoundary() {
+	return (
+		<html lang="en">
+			<head>
+				<title>Oh no!</title>
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<div className="flex items-center max-w-4xl  mx-auto h-screen justify-center">
+					<div className="flex flex-col items-center space-y-3">
+						<img src="/sym.svg" width={240} className="block" alt="Compa" />
+						<h1 className="font-bold text-3xl">Sorry, Unexpected Error</h1>
+						<p className="text-gray-500 max-w-2xl text-center">
+							We are facing an internal server error. Our expects are trying to
+							fix the problem. Please try again or wait for sometime
+						</p>
+						<a
+							href="/discussions"
+							className="bg-gray-800 px-3 py-4 rounded-md tracking-wide font-bold text-gray-500"
+						>
+							{" "}
+							TAKE ME HOME
+						</a>
+					</div>
+				</div>
+				<Scripts />
+			</body>
+		</html>
+	);
+}
+
 export default function App() {
 	const { user } = useLoaderData<typeof loader>();
 
@@ -50,7 +82,10 @@ export default function App() {
 				/>
 				<link rel="manifest" href="/manifest.webmanifest" />
 				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
-				<link href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css" rel="stylesheet" />
+				<link
+					href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"
+					rel="stylesheet"
+				/>
 				<Meta />
 				<Links />
 			</head>
