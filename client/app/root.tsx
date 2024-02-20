8c991fb1f1f875bf5a6e7323ab864318fd3aa99a
Degreat <mail@degreat.co.uk>
2024-02-20 17:45:47 +0000

    restyle and add dev logs

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -26,6 +26,7 @@ import { User } from "@prisma/client";
 import { Anchor } from "./components/anchor";
 import posthog from "posthog-js";
 import { useEffect } from "react";
+import { SymOutline } from "./components/sym-outline";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
 	let user: User | undefined | null;
@@ -49,6 +50,8 @@ export function ErrorBoundary() {
 	const statusCode = isRouteErrorResponse(error) ? error.status : 600;
 
 	useEffect(() => {
+		if (statusCode === 404) return;
+
 		if (isRouteErrorResponse(error)) {
 			posthog.capture("route-error-response", {
 				data: error.data,
@@ -63,6 +66,8 @@ export function ErrorBoundary() {
 		}
 	}, [error, statusCode, location]);
 
+	const stackTrace = (error as Error).stack;
+
 	return (
 		<html lang="en">
 			<head>
@@ -74,31 +79,47 @@ export function ErrorBoundary() {
 				<Meta />
 				<Links />
 			</head>
-			<body className="flex flex-col min-h-screen">
-				<div className="flex-1">
-					<div className="flex items-start justify-center space-x-4 p-3  py-38">
-						<img src="/sym.svg" width={80} alt="Compa" />
+			<body>
+				<Navbar />
 
-						<div className="flex flex-col items-start space-y-4 ">
+				<div className="min-h-[60vh] mt-8">
+					<div className="flex mx-auto gap-4 max-w-[28rem] px-4">
+						<div>
+							<SymOutline className="size-12" />
+						</div>
+
+						<div>
 							<h1 className="font-bold">
 								{statusCode === 404
-									? "404 : Not found"
-									: `${statusCode} : An unexpected error occured`}
+									? "404: Not found"
+									: `${statusCode}: An unexpected error occured`}
 							</h1>
-							<p className="text-gray-500 w-1/2">
+
+							<p className="text-gray-500">
 								{statusCode === 404
 									? "There is nothing on this page. Perhaps, what was on this page is long gone. Sorry!"
-									: "This must be strange to you. It's strange to us too. we will look into this and resolve it as soon as possible"}
+									: "This must be strange to you. It's strange to us too. we will look into this and resolve it as soon as possible."}
 							</p>
-							<Anchor href="/discussions">
-								Go Home <div className="i-lucide-arrow-right  opacity-50"></div>
-							</Anchor>
+
+							<div className="mt-2">
+								<Anchor href="/discussions">
+									Go Home <div className="i-lucide-arrow-right opacity-50" />
+								</Anchor>
+							</div>
 						</div>
 					</div>
+
+					{process.env.NODE_ENV === "development" && stackTrace && (
+						<div className="p-4 border-t mt-8">
+							<header className="flex font-mono bg-red-600 text-white items-center inline-flex rounded-lg px-2 text-sm gap-2">
+								<div className="i-lucide-eye opacity-70" /> Dev only
+							</header>
+							<pre className="text-sm text-gray-500 mt-4">{stackTrace}</pre>
+						</div>
+					)}
 				</div>
 
 				<Footer />
-
 				<Scripts />
 			</body>
 		</html>
