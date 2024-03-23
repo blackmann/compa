ebb721e371f3e0dd7db6fd5819f320b9b1fd56bf
kwadwoatta <princeofori065@gmail.com>
2024-03-23 14:18:24 +0000

    fix(ts-errors): 3 down

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -1,85 +1,85 @@
 import "@unocss/reset/tailwind.css";
 import "./style.css";
 
+import { User } from "@prisma/client";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import {
-	Links,
-	LiveReload,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-	json,
-	useLoaderData,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  json,
+  useLoaderData,
 } from "@remix-run/react";
-import { BottomNav, Navbar } from "./components/navbar";
+import React from "react";
+import { CommonHead } from "./components/common-head";
 import { Footer } from "./components/footer";
+import { BottomNav, Navbar } from "./components/navbar";
 import { PendingUI } from "./components/pending-ui";
 import { checkAuth } from "./lib/check-auth";
-import { prisma } from "./lib/prisma.server";
 import { GlobalCtx } from "./lib/global-ctx";
-import { User } from "@prisma/client";
-import { CommonHead } from "./components/common-head";
+import { prisma } from "./lib/prisma.server";
 import { useColorScheme } from "./lib/use-color-scheme";
-import React from "react";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-	let user: User | undefined | null;
-	try {
-		const userId = await checkAuth(request);
-		user = await prisma.user.findFirst({ where: { id: userId } });
-	} catch (error) {
-		//
-	}
+  let user: User | undefined | null;
+  try {
+    const userId = await checkAuth(request);
+    user = await prisma.user.findFirst({ where: { id: userId } });
+  } catch (error) {
+    //
+  }
 
-	return json({ user });
+  return json({ user });
 };
 
 export const links: LinksFunction = () => [
-	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export { ErrorBoundary } from "./components/error-boundary";
 
 export default function App() {
-	const { user } = useLoaderData<typeof loader>();
-	const scheme = useColorScheme();
+  const { user } = useLoaderData<typeof loader>();
+  const scheme = useColorScheme();
 
-	React.useEffect(() => {
-		if (scheme === "light") {
-			// dynamically change meta theme-color
-			document
-				.querySelector('meta[name="theme-color"]')
-				?.setAttribute("content", "#FAFAFA");
-		} else {
-			document
-				.querySelector('meta[name="theme-color"]')
-				?.setAttribute("content", "#171717");
-		}
-	}, [scheme]);
+  React.useEffect(() => {
+    if (scheme === "light") {
+      // dynamically change meta theme-color
+      document
+        .querySelector('meta[name="theme-color"]')
+        ?.setAttribute("content", "#FAFAFA");
+    } else {
+      document
+        .querySelector('meta[name="theme-color"]')
+        ?.setAttribute("content", "#171717");
+    }
+  }, [scheme]);
 
-	return (
-		<html lang="en">
-			<head>
-				<CommonHead />
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				<PendingUI />
+  return (
+    <html lang="en">
+      <head>
+        <CommonHead />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <PendingUI />
 
-				<GlobalCtx.Provider value={{ user }}>
-					<Navbar />
-					<Outlet context={{ user }} />
-				</GlobalCtx.Provider>
+        <GlobalCtx.Provider value={{ user: user as User | null | undefined }}>
+          <Navbar />
+          <Outlet context={{ user }} />
+        </GlobalCtx.Provider>
 
-				<ScrollRestoration />
-				<Scripts />
-				<LiveReload />
-				<Footer />
-				<BottomNav />
-			</body>
-		</html>
-	);
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+        <Footer />
+        <BottomNav />
+      </body>
+    </html>
+  );
 }
