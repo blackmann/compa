87f2ce3a486cdefc6ea760f20eb7b1de28e4cc85
kwadwoatta <princeofori065@gmail.com>
2024-03-23 14:33:37 +0000

    chore(.vscode): add to extensions and settings

--- client/app/components/file-menu.tsx
+++ client/app/components/file-menu.tsx
@@ -4,37 +4,37 @@ import { useGlobalCtx } from "~/lib/global-ctx";
 import { DropdownMenu } from "./dropdown-menu";
 
 interface Props {
-	file: Prisma.RepositoryGetPayload<{ include: { user: true } }>;
+  file: Prisma.RepositoryGetPayload<{ include: { user: true } }>;
 }
 
 function FileMenu({ file }: Props) {
-	const { user } = useGlobalCtx();
-	const fetcher = useFetcher();
+  const { user } = useGlobalCtx();
+  const fetcher = useFetcher();
 
-	if (user?.id !== file.userId) {
-		return <div className="size-8" />;
-	}
+  if (user?.id !== file.userId) {
+    return <div className="size-8" />;
+  }
 
-	const menuItems = [
-		{ id: "delete-file", title: "Delete file", icon: "i-lucide-trash" },
-	];
+  const menuItems = [
+    { id: "delete-file", title: "Delete file", icon: "i-lucide-trash" },
+  ];
 
-	function handleClick(actionId: string) {
-		if (actionId === "delete-file") {
-			const yes = confirm(
-				"Are you sure you want to delete this file? This cannot be undone.",
-			);
+  function handleClick(actionId: string) {
+    if (actionId === "delete-file") {
+      const yes = confirm(
+        "Are you sure you want to delete this file? This cannot be undone."
+      );
 
-			if (yes) {
-				fetcher.submit("", {
-					method: "DELETE",
-					action: `/library/${file.id}`,
-				});
-			}
-		}
-	}
+      if (yes) {
+        fetcher.submit("", {
+          method: "DELETE",
+          action: `/library/${file.id}`,
+        });
+      }
+    }
+  }
 
-	return <DropdownMenu items={menuItems} onItemClick={handleClick} />;
+  return <DropdownMenu items={menuItems} onItemClick={handleClick} />;
 }
 
 export { FileMenu };
--- client/app/entry.client.tsx
+++ client/app/entry.client.tsx
@@ -5,27 +5,27 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
-import posthog from "posthog-js";
 import AudioRecorder from "audio-recorder-polyfill";
 import mpegEncoder from "audio-recorder-polyfill/mpeg-encoder";
+import posthog from "posthog-js";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
 
 AudioRecorder.encoder = mpegEncoder;
 AudioRecorder.prototype.mimeType = "audio/mpeg";
 window.MediaRecorder = AudioRecorder;
 
 if (process.env.NODE_ENV === "production") {
-	posthog.init("phc_qmxF7NTz6XUnYUDoMpkTign6mujS8F8VqR75wb0Bsl7", {
-		api_host: "https://eu.posthog.com",
-	});
+  posthog.init("phc_qmxF7NTz6XUnYUDoMpkTign6mujS8F8VqR75wb0Bsl7", {
+    api_host: "https://eu.posthog.com",
+  });
 }
 
 startTransition(() => {
-	hydrateRoot(
-		document,
-		<StrictMode>
-			<RemixBrowser />
-		</StrictMode>,
-	);
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <RemixBrowser />
+    </StrictMode>
+  );
 });
