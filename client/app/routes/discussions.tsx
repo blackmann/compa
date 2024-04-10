7e904fa5be1dfa4512824a9cdf521187e990178e
Degreat <mail@degreat.co.uk>
2024-04-10 19:51:26 +0000

    border light

--- client/app/routes/discussions.tsx
+++ client/app/routes/discussions.tsx
@@ -190,7 +190,7 @@ function Paginated({ fromDate }: PaginatedProps) {
 				</div>
 			) : (
 				<button
-					className="px-2 py-1 rounded-lg border font-medium inline-flex items-center gap-2 text-secondary hover:bg-zinc-100 transition-[background] duration-200"
+					className="px-2 py-1 rounded-lg border dark:border-neutral-700 font-medium inline-flex items-center gap-2 text-secondary hover:bg-zinc-100 transition-[background] duration-200"
 					onClick={handleLoadMore}
 					type="button"
 				>
