c64f08d297d2619daba2cff11b0f96ace32b4457
Degreat <mail@degreat.co.uk>
2024-04-11 19:10:41 +0000

    better dark

--- client/app/components/modal.tsx
+++ client/app/components/modal.tsx
@@ -32,7 +32,7 @@ function Modal({ children, className, onClose, open }: Props) {
 	return ReactDOM.createPortal(
 		<dialog
 			className={clsx(
-				"rounded-xl border border-zinc-300 dark:border-neutral-700 shadow",
+				"rounded-xl border border-zinc-300 dark:border-neutral-700 shadow dark:bg-neutral-900",
 				className,
 			)}
 			ref={ref}
