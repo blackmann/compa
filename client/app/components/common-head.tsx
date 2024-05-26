4a3e0a324aa2cb280f94e9a6cb4538742e8952b1
Degreat <mail@degreat.co.uk>
2024-05-26 17:24:55 +0000

    observe

--- client/app/components/common-head.tsx
+++ client/app/components/common-head.tsx
@@ -1,3 +1,5 @@
+const dev = import.meta.env.NODE_ENV !== "production";
+
 function CommonHead() {
 	return (
 		<>
@@ -6,7 +8,7 @@ function CommonHead() {
 				name="viewport"
 				content="width=device-width,initial-scale=1,maximum-scale=1,viewport-fit=cover"
 			/>
-			<meta name="theme-color" content="#FAFAFA"/>
+			<meta name="theme-color" content="#FAFAFA" />
 			<link rel="manifest" href="/manifest.webmanifest" />
 			<link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
 			<link rel="icon" type="image/png" href="/favicon.png" />
@@ -14,8 +16,14 @@ function CommonHead() {
 				href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"
 				rel="stylesheet"
 			/>
+			{!dev && (
+				<script
+					src="https://0.observe.so/script.js"
+					data-app="clwnp4jnp0000ze4wmdyiawle"
+				/>
+			)}
 		</>
 	);
 }
 
-export { CommonHead }
+export { CommonHead };
