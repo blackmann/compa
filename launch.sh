fb2b58f2adb16fb2441a1c674ba4c7f5ca261b01
Degreat <mail@degreat.co.uk>
2024-05-30 07:59:17 +0000

    launch ug

---
+++ launch.sh
@@ -0,0 +1,19 @@
+dokku apps:create $1
+
+COOKIE_SECRET_1=$(openssl rand -hex 16)
+COOKIE_SECRET_2=$(openssl rand -hex 16)
+dokku config:set $1 SCHOOL=$1
+dokku config:set $1 COOKIE_SECRET=$COOKIE_SECRET_1,$COOKIE_SECRET_2
+
+SECRET_KEY=$(openssl rand -hex 16)
+dokku config:set $1 SECRET_KEY=$SECRET_KEY
+dokku config:set $1 RESEND_API_KEY=$RESEND_API_KEY
+
+dokku config:set $1 AWS_UPLOAD_ENDPOINT="eu-central-1.linodeobjects.com"
+dokku config:set $1 AWS_REGION="eu-central-1"
+dokku config:set $1 AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+dokku config:set $1 AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+dokku config:set $1 AWS_BUCKET=compa
+dokku config:set $1 AWS_BUCKET_DIR=$1-compa
+
+dokku domains:set $1 $1.compa.so
