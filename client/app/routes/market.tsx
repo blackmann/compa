c0823db47243c24325ca1a5a659e96a8207e0192
lasagna <arthurebenezer@aol.com>
2024-07-04 22:49:11 +0000

    fix: search/filter functionality in marketplace

--- client/app/routes/market.tsx
+++ client/app/routes/market.tsx
@@ -1,13 +1,20 @@
 import type { User } from "@prisma/client";
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import { useLoaderData, useRouteLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { prisma } from "~/lib/prisma.server";
+import { checkAuth } from "~/lib/check-auth";
+import { values } from "~/lib/values.server";
+import { useState, useEffect } from "react";
+import {
+	useLoaderData,
+	useRouteLoaderData,
+	useFetcher,
+	useSearchParams,
+} from "@remix-run/react";
 import { Anchor } from "~/components/anchor";
 import { Input } from "~/components/input";
 import { ProductItem } from "~/components/product-item";
 import { Select } from "~/components/select";
-import { checkAuth } from "~/lib/check-auth";
-import { prisma } from "~/lib/prisma.server";
-import { values } from "~/lib/values.server";
 import type { loader as rootLoader } from "~/root";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -16,7 +23,9 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 	try {
 		const userId = await checkAuth(request);
 		user = await prisma.user.findFirst({ where: { id: userId } });
-	} catch {}
+	} catch {
+		//
+	}
 
 	const sellerProfile = user
 		? await prisma.sellerProfile.findFirst({
@@ -24,15 +33,65 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 			})
 		: null;
 
+	const url = new URL(request.url);
+	const category = url.searchParams.get("category");
+	const searchQuery = url.searchParams.get("query") || "";
+
+	const categoryFilter =
+		category && category !== "all"
+			? { categoryId: Number.parseInt(category, 10) }
+			: {};
+
 	const products = await prisma.product.findMany({
-		where: { status: "available" },
+		where: {
+			status: "available",
+			...(searchQuery
+				? {
+						OR: [
+							{ name: { contains: searchQuery } },
+							{ description: { contains: searchQuery } },
+						],
+					}
+				: {}),
+			...categoryFilter,
+		},
 		include: { category: true },
 		orderBy: { createdAt: "desc" },
 	});
 
 	const categories = await prisma.category.findMany();
 
-	return { categories, school: values.meta(), products, sellerProfile };
+	return json({ categories, school: values.meta(), products, sellerProfile });
+};
+
+export const action = async ({ request }: LoaderFunctionArgs) => {
+	const formData = await request.formData();
+	const searchQuery = (formData.get("query") as string) || "";
+	const category = (formData.get("category") as string) || undefined;
+
+	const categoryFilter =
+		category && category !== "all"
+			? { categoryId: Number.parseInt(category, 10) }
+			: {};
+
+	const products = await prisma.product.findMany({
+		where: {
+			status: "available",
+			...(searchQuery
+				? {
+						OR: [
+							{ name: { contains: searchQuery } },
+							{ description: { contains: searchQuery } },
+						],
+					}
+				: {}),
+			...categoryFilter,
+		},
+		include: { category: true },
+		orderBy: { createdAt: "desc" },
+	});
+
+	return json(products);
 };
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
@@ -46,18 +105,65 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
 };
 
 export default function Market() {
-	const { categories, products, sellerProfile } =
-		useLoaderData<typeof loader>();
+	const {
+		categories,
+		products: initialProducts,
+		sellerProfile,
+	} = useLoaderData<typeof loader>();
 	const { user } = useRouteLoaderData<typeof rootLoader>("root") || {};
+	const [products, setProducts] = useState(initialProducts);
+	const [params] = useSearchParams();
+	const fetcher = useFetcher();
+	const [searchQuery, setSearchQuery] = useState(params.get("query") || "");
+	const [selectedCategory, setSelectedCategory] = useState(
+		params.get("category") || "all",
+	);
+	const [searchPerformed, setSearchPerformed] = useState<boolean>(false);
+
+	const handleSearchChange = (
+		event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+	) => {
+		const { name, value } = event.target;
+
+		if (name === "query") {
+			setSearchQuery(value);
+		} else if (name === "category") {
+			setSelectedCategory(value);
+		}
+	};
+
+	useEffect(() => {
+		const formData = new FormData();
+		formData.set("query", searchQuery);
+		formData.set("category", selectedCategory);
+		fetcher.submit(formData, { method: "post" });
+		setSearchPerformed(true);
+	}, [searchQuery, selectedCategory]);
+
+	useEffect(() => {
+		if (fetcher.data) {
+			setProducts(Array.isArray(fetcher.data) ? fetcher.data : initialProducts);
+		}
+	}, [fetcher.data, initialProducts]);
 
 	return (
 		<div className="container min-h-[60vh]">
 			<header className="mb-2">
 				<h1 className="font-bold text-xl">Marketplace</h1>
 
-				<div className="flex items-start gap-2">
-					<Input type="search" placeholder="Search product" />
-					<Select>
+				<fetcher.Form className="flex items-start gap-2">
+					<Input
+						name="query"
+						value={searchQuery}
+						onChange={handleSearchChange}
+						type="search"
+						placeholder="Search product"
+					/>
+					<Select
+						name="category"
+						value={selectedCategory}
+						onChange={handleSearchChange}
+					>
 						<option value="all">All Categories</option>
 						{categories.map((category) => (
 							<option key={category.id} value={category.id}>
@@ -65,7 +171,7 @@ export default function Market() {
 							</option>
 						))}
 					</Select>
-				</div>
+				</fetcher.Form>
 			</header>
 
 			{sellerProfile && (
@@ -102,17 +208,28 @@ export default function Market() {
 				</div>
 			)}
 
-			<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-				{products.map((product) => {
-					// [ ] Empty state for products
-
-					return (
+			{products.length === 0 && searchPerformed ? (
+				<div className="text-center text-secondary">
+					No items match "{searchQuery}"{" "}
+					{selectedCategory !== "all" && "in this category"}
+				</div>
+			) : (
+				<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+					{products.map((product) => (
 						<div className="col-span-1" key={product.id}>
 							<ProductItem product={product} />
 						</div>
-					);
-				})}
-			</div>
+					))}
+				</div>
+			)}
+
+			{products.length === 0 &&
+				selectedCategory !== "all" &&
+				!searchPerformed && (
+					<div className="text-center text-secondary">
+						No items in this category
+					</div>
+				)}
 		</div>
 	);
 }
